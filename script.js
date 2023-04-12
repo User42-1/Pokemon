8d45b005a0e86a6dbf957e2e256ecf633ee09732
User42-1 <jens.schrecker@t-online.de>
2023-04-12 14:54:58 +0200

    infocard works with details an onclick + stopPropagation()

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let allPokemonObjects = [];   // #1 is in array 0 and so on
 async function init() {
     await load_allPokemonNamesUrls();
     await load_first30_pokemonObjects();
-    /* await load_further20_pokemonObjects(31); */  // is called when the page is scrolled downwords
+    /* await load_further20_pokemonObjects(31); */  // is called with a button (or when the page is scrolled downwords - not implemented yet)
     await render_first31_pokemon_overview();
 }
 
@@ -47,37 +47,48 @@ async function render_first31_pokemon_overview() {
 }
 
 function show_infocard_container(i) {
-/*     document.getElementById('infocard_container').classList.toggle('d_none');
- */
     let content_infocard = document.getElementById('infocard_container');
     content_infocard.classList.remove('d_none');
+ /* content_infocard.classList.toggle('d_none'); */
     let pokemont_sprite_name = allPokemonObjects[i]['name'];
+    let pokemont_sprite_src = allPokemonObjects[i]['sprites']['other']['official-artwork']['front_shiny'];
+    let pokemont_sprite_type = allPokemonObjects[i]['types'][0]['type']['name'];
+    let pokemont_sprite_weight = allPokemonObjects[i]['weight'];
+    let pokemont_sprite_height = allPokemonObjects[i]['height'];
+    let pokemont_sprite_stats0 = allPokemonObjects[i]['stats'][0]['base_stat'];
+    let pokemont_sprite_stats1 = allPokemonObjects[i]['stats'][1]['base_stat'];
+    let pokemont_sprite_stats2 = allPokemonObjects[i]['stats'][2]['base_stat'];
     content_infocard.innerHTML += `
         <div id="infocard_container" class="infocard_container">
             <div class="infocard_upperPart">
-                <h2 id="pokemon_name">${pokemont_sprite_name}</h2>
+                <h1 id="pokemon_name">${pokemont_sprite_name}</h1>
+                <div class="small_sprite_container"><img src="${pokemont_sprite_src}" class="pokemon_sprite_small_image"></div>
             </div>
             <div class="infocard_lowerPart">
                 <table>
                     <tr>
-                        <td>Name:</td><td id="name">name</td>
+                        <td>Type:</td><td id="type">${pokemont_sprite_type}</td>
                     </tr>
                     <tr>
-                        <td>Type:</td><td id="type">type</td>
+                        <td>Height:</td><td id="height">${pokemont_sprite_height} dm</td>
                     </tr>
                     <tr>
-                        <td>Height:</td><td id="height">height</td>
+                        <td>Weight:</td><td id="weight">${pokemont_sprite_weight} kg</td>
                     </tr>
                     <tr>
-                        <td>Weight:</td><td id="weight">weight</td>
+                        <td>Hp:</td><td id="abilities">${pokemont_sprite_stats0} Hp</td>
                     </tr>
                     <tr>
-                        <td>Abilities:</td><td id="abilities">abilities</td>
+                        <td>Attack:</td><td id="abilities">${pokemont_sprite_stats1} At</td>
+                    </tr>
+                    <tr>
+                        <td>Defense:</td><td id="abilities">${pokemont_sprite_stats2} De</td>
                     </tr>
                 </table>
             </div>
         </div>   
         `;
+        event.stopPropagation();
 }
 
 async function render_further20_pokemon_overview(pokemonStartId) {
@@ -86,57 +97,13 @@ async function render_further20_pokemon_overview(pokemonStartId) {
         const pokemont_sprite_src = allPokemonObjects[i]['sprites']['other']['official-artwork']['front_shiny'];
         const pokemont_sprite_name = allPokemonObjects[i]['name'];
         content_container.innerHTML += `<div class="pokemon_sprite_container">
-                                <img src="${pokemont_sprite_src}" class="pokemon_sprite_image" onclick="show_infocard_container()">
+                                <img src="${pokemont_sprite_src}" class="pokemon_sprite_image" onclick="show_infocard_container(${i})">
                                 <div class="pokeman_sprite_name">${pokemont_sprite_name}</div>
                             </div>
                             `;
     }
 }
 
-/* async function render_infocard() {
-    let content = document.getElementById('infocard_container');
-} */
-
-
-
-function remove_infocard_container() {
+async function remove_infocard_container() {
     document.getElementById('infocard_container').classList.add('d_none');
 }
-
-/* 
-addEventListener(onmousedown, () => {}) */
-
-/////////////////////////// Test with 4 given Pokemon /////////////////////////////////////////
-
-/* let all_pokemon = [];
-
-let pokemons = ['pikachu', 'charmander', 'pikachu', 'wigglytuff'];
-
-async function load_pokemons() {
-    for (let i = 0; i < pokemons.length; i++) {
-        const pokemon = pokemons[i];
-    let url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
-    let response = await fetch(url);
-    let current_pokemon = await response.json();
-    all_pokemon.push(current_pokemon);
-    console.log('loaded pokemon:' , current_pokemon);
-    render_pokemon_infocard(current_pokemon);
-    }
-}
-
-function render_pokemon_infocard(current_pokemon) {
-    /*   for (let i = 0; i < pokemons.length; i++) {
-          const pokemon = pokemons[i];
-  
-          document.getElementById('small_charts').innerHTML = current_pokemon['name']; */
-/*       document.getElementById('pokemon_name').innerHTML = current_pokemon['name'];
-      document.getElementById('pokemon_small_image').src = current_pokemon['sprites']['other']['official-artwork']['front_shiny'];
-      document.getElementById('type').innerHTML = allPokemonObjects['types']['other']['official-artwork']['front_shiny'];
-      document.getElementById('height').src = current_pokemon['sprites']['other']['official-artwork']['front_shiny'];
-      document.getElementById('weight').src = current_pokemon['sprites']['other']['official-artwork']['front_shiny'];
-  }
- */
-
-
-/*   document.getElementById('pokemon_sprite').src = await allPokemonObjects[1]['sprites']['other']['official-artwork']['front_shiny'];
- */
